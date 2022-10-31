6be4f3d604b76372dc67f8bf16b442772729fd56
MrSeadev <biencoder@gmail.com>
2022-10-31 10:45:39 +0700

    feat: add Post model

---
+++ app/Post.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title', 'content', 'user_id'
+    ];
+
+    /**
+     * A post belongs to a user
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+}
