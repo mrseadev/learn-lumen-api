60c22a6a94a14f99d6f494f738daa547fc550027
MrSeadev <biencoder@gmail.com>
2022-11-04 03:14:54 +0700

    feat: add API version

--- routes/web.php
+++ routes/web.php
@@ -11,23 +11,36 @@
 |
 */
 
+define("API_VERSION", "v1");
+define("API_PREFIX", "api/" . API_VERSION);
+
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
-$router->group(['prefix' => 'api/'], function () use ($router) {
-    $router->post('users/add', 'UserController@add');
-    $router->get('users/login', 'UserController@login');
-    $router->get('users/refresh_token', ['middleware' => 'auth', 'uses' => 'UserController@refreshToken']);
-    $router->get('users/{id}/posts', ['middleware' => 'auth', 'uses' => 'PostController@getUserPosts']);
+$router->group(['prefix' => API_PREFIX . "/users"], function () use ($router) {
+    $router->post('/add', 'UserController@add');
+    $router->get('/login', 'UserController@login');
+    $router->get('/refresh_token', ['middleware' => 'auth', 'uses' => 'UserController@refreshToken']);
+    $router->get('/{id}/posts', ['middleware' => 'auth', 'uses' => 'PostController@getUserPosts']);
+});
 
-    $router->group(['prefix' => 'posts/', 'middleware' => 'auth'], function () use ($router) {
-        $router->get('', 'PostController@gets');
-        $router->get('{id}', 'PostController@get');
-        $router->post('add', 'PostController@add');
-        $router->put('{id}/update', 'PostController@update');
-        $router->post('{id}/duplicate', 'PostController@duplicate');
-        $router->delete('{id}/delete', 'PostController@delete');
-        $router->delete('delete', 'PostController@deleteAll');
-    });
+$router->group(['prefix' => API_PREFIX . "/posts", 'middleware' => 'auth'], function () use ($router) {
+    $router->get('', 'PostController@gets');
+    $router->get('/{id}', 'PostController@get');
+    $router->post('/add', 'PostController@add');
+    $router->put('/{id}/update', 'PostController@update');
+    $router->post('/{id}/duplicate', 'PostController@duplicate');
+    $router->delete('/{id}/delete', 'PostController@delete');
+    $router->delete('/delete', 'PostController@deleteAll');
+});
+
+$router->group(['prefix' => API_PREFIX . "/todos", 'middleware' => 'auth'], function () use ($router) {
+    $router->get('', 'TodoController@index');
+    $router->get('/{id}', 'TodoController@get');
+    $router->post('/add', 'TodoController@add');
+    $router->put('/{id}/update', 'TodoController@update');
+    $router->post('/{id}/duplicate', 'TodoController@duplicate');
+    $router->delete('/{id}/delete', 'TodoController@delete');
+    $router->delete('/delete', 'TodoController@deleteAll');
 });
